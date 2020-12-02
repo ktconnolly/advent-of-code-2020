9c2ff3b06099e3be5400b4d17d619a67262f603f
Kerwin Connolly <kerwin.connolly@thehutgroup.com>
2020-12-02 13:46:05 +0000

    Refactor

--- day_02.py
+++ day_02.py
@@ -6,21 +6,19 @@ def parse_policy(line):
     return match.group(1), match.group(2), match.group(3), match.group(4)
 
 
-def is_valid_part_one(line):
-    minimum, maximum, char, password = parse_policy(line)
-    return int(minimum) <= password.count(char) <= int(maximum)
-
-
-def is_valid_part_two(line):
-    pos_1, pos_2, char, password = parse_policy(line)
-    return (password[int(pos_1) - 1] == char) != (password[int(pos_2) - 1] == char)
-
-
 def part_one():
+    def is_valid(line):
+        minimum, maximum, char, password = parse_policy(line)
+        return int(minimum) <= password.count(char) <= int(maximum)
+
     with open("inputs/day_02.txt", "r") as f:
-        return sum(is_valid_part_one(l.strip()) for l in f)
+        return sum(is_valid(l.strip()) for l in f)
 
 
 def part_two():
+    def is_valid(line):
+        pos_1, pos_2, char, password = parse_policy(line)
+        return (password[int(pos_1) - 1] == char) != (password[int(pos_2) - 1] == char)
+
     with open("inputs/day_02.txt", "r") as f:
-        return sum(is_valid_part_two(l.strip()) for l in f)
+        return sum(is_valid(l.strip()) for l in f)
